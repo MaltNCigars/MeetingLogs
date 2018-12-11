410f158e1619fa408624adaee7583f0cda67f931
Michael James Hon <mhon2014@my.fit.edu>
2018-12-10 22:52:57 -0500

    updated 15424 logs

--- 15424_test_log/sympycodetest.py
+++ 15424_test_log/sympycodetest.py
@@ -1,3 +1,3 @@
 import sympy as sy
 symbols = sy.symbols('a[0:50]')
-sy.ccode(sy.Min(*symbols))
+print sy.ccode(sy.Min(*symbols))
--- 15424_test_log/sympydoit.py
+++
@@ -1,4 +0,0 @@
-from sympy import pprint, symbols, Integral, sin, pi
-
-x = symbols('x')
-Integral(abs(sin(x)), (x, 0, 7 * pi)).doit()
